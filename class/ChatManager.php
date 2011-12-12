80a5c5764ad51cfbd579b5f3b14d3784a788130b
Angelo Rodrigues <=>
2011-12-12 09:38:07 -0500

    fixed chat manager for internal messages

--- class/ChatManager.php
+++ class/ChatManager.php
@@ -6,8 +6,13 @@ class ChatManager {
   private $time;
   private $player;
 
-  public function __construct($message) {
-    $this->player = unserialize($_SESSION['player']);
+  public function __construct($message, $from = null) {
+  	if(empty($from)) {
+    	$this->player = unserialize($_SESSION['player']);
+  	}
+  	else {
+  		$this->player = $from;
+  	}
     
     $this->time = time();
     
@@ -39,18 +44,40 @@ class ChatManager {
     $toplayer = R::findOne('player','username = ?',array($to));
     
     if(!empty($toplayer)) {
-      $this->msg->touser = $toplayer->username;
-      $this->msg->text = trim($message);
-      $this->msg->classification = 0;
+	    $this->msg->touser = $toplayer->username;
+	    $this->msg->text = trim($message);
+	    $this->msg->classification = 0;
     }
     else {
-      // send only that user a message saying they failed
-      $this->msg->fromuser = 'Server';
-      $this->msg->touser = $this->player->username;
-      $this->msg->text = 'Your message could not be sent to '.$to;
-      $this->msg->classification = 2;
+    	if(strtolower($to) == 'server') {
+    		// if the user pm's Server it logs a "report" which we can see from
+    		// the administration panel. At
+    		$this->generate_report($message);
+    	}
+    	else {
+	      // send only that user a message saying they failed
+		    $this->msg->fromuser = 'Server';
+		    $this->msg->touser = $this->player->username;
+	      $this->msg->text = 'User \''.$to.'\' does not exist.';
+	     	$this->msg->classification = 2;
+    	}
     }
-
+  }
+  
+  public function generate_report($message) {
+  	$report = R::dispense('report');
+  	$report->fromuser = $this->msg->fromuser;
+  	$report->message = $message;
+  	$report->post_time = $this->msg->post_time;
+  	
+  	$report_id = R::store($report);
+  	
+  	$this->msg->fromuser = 'Server';
+  	$this->msg->touser = $this->player->username;
+  	$this->msg->text = 'A report has been filed! Your case is: #'.$report_id;
+  	$this->msg->classification = 2;
+  	
+  	// at some point, an email will be generated and sent out to mods.
   }
 
   public function execute() {
